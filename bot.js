95e02e527719ac55e15227dd671b8c9f8d5068ba
zeiadamr <45317405+zeiadamr@users.noreply.github.com>
2019-05-21 05:54:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**#credits **[ " ${x} " ]`)
+        message.channel.send(`**#credits **`)
           .then(m => {
             count++;
           })
@@ -72,7 +72,7 @@ if (message.content === '!spam') {
       let count = 0;
       let ecount = 0;
       for(let x = 0; x < 90000; x++) {
-        message.channel.send(`**credits **[ " ${x} " ]`)
+        message.channel.send(`**#credits **`)
           .then(m => {
             count++;
           })
